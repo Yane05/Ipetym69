4d478d771edb40669b83383c61550c6a6c86ba7c
Paulaurbinatti <paulaurbinatti@gmail.com>
2022-07-09 01:43:49 -0300

    react

--- Proyecto react/ipetym69/src/pages/lista/Lista.js
+++ Proyecto react/ipetym69/src/pages/lista/Lista.js
@@ -10,56 +10,55 @@ function Lista() {
 
     useEffect(() => {
         buscarDocentes();
-    },[]);
+    }, []);
 
     const buscarDocentes = async () => {
+        let url = 'http://localhost:8080/Ipetym69/docenteServlet';
+        let respuesta = await fetch(url, {
+            "method": 'GET',
+            "headers": {
+                "Accept": 'application/json',
+                "Content-Type": 'application/json' 
+            }
+        })
 
-    let respuesta = await fetch ("http://localhost:8080/Ipetym69/docenteServlet", {
-        "method": 'GET',
-        "headers": {
-            "Accept": 'application/json',
-            "Content-Type": 'application/json'
-        }})
-    let json = respuesta.json();
-    setDocentes(json);
+        console.log(respuesta);
+        let json = respuesta.json();
+        setDocentes(json);
+
     }
 
     const datos =
     {
         columns: [
             {
-                label: 'Docente',
-                field: 'docente',
+                label: 'Nombre',
+                field: 'nombre',
                 sort: 'asc'
             },
             {
-                label: 'Materia',
-                field: 'materia',
+                label: 'Apellido',
+                field: 'apellido',
                 sort: 'asc'
             },
             {
-                label: 'Curso',
-                field: 'curso',
+                label: 'Dni',
+                field: 'dni',
                 sort: 'asc'
             },
             {
-                label: 'División',
-                field: 'division',
+                label: 'Telefono',
+                field: 'telefono',
                 sort: 'asc'
             },
             {
-                label: 'Día',
-                field: 'dia',
+                label: 'Email',
+                field: 'email',
                 sort: 'asc'
             },
             {
-                label: 'Horario',
-                field: 'horario',
-                sort: 'asc'
-            },
-            {
-                label: 'Turno',
-                field: 'turno',
+                label: 'Legajo',
+                field: 'legajo',
                 sort: 'asc'
             }
         ],
@@ -69,17 +68,17 @@ function Lista() {
     return (
         <div className="lista">
             <h1 className='titulo'>Escuela IPETYM N°69</h1>
-            
-                <button className="button"><a className='link' href="">Nuevo Docente</a></button>
-                <button className="button"><a className='link' href="">Nueva Materia</a></button>
-                <br /><br />
 
-                <MDBDataTable
-                    striped
-                    bordered
-                    data={datos}
-                />
-       
+            <button className="button"><a className='link' href="">Nuevo Docente</a></button>
+            <button className="button"><a className='link' href="">Nueva Materia</a></button>
+            <br /><br />
+
+            <MDBDataTable
+                striped
+                bordered
+                data={datos}
+            />
+
         </div>
     );
 }
